e1aea80633c6efea45803e8905a89a5a009b2d39
xthecapx <xthecapx@gmail.com>
2024-09-25 13:16:31 -0500

    fix issue with local dev

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/quantum-react-js/'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/quantum-react-js/' : '/'
+  }
 })
