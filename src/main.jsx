6d184e3570c6a9c2c391c3b57a6922b1b43e8eda
xthecapx <xthecapx@gmail.com>
2024-09-25 13:35:01 -0500

    added material ui

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import App from './App.jsx';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import DescriptionIcon from '@mui/icons-material/Description';
+import LayersIcon from '@mui/icons-material/Layers';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+
 
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './Home.jsx';
@@ -25,11 +35,31 @@ const darkTheme = createTheme({
   },
 });
 
+const NAVIGATION = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    segment: 'dashboard',
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  },
+];
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <AppProvider
+      navigation={NAVIGATION}
+      router={router}
+      theme={darkTheme}
+      window={window !== undefined ? window() : undefined}
+      >
+      <DashboardLayout>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </DashboardLayout>
+    </AppProvider>
   </StrictMode>
 );
